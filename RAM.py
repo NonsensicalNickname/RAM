f260b3c9c99006c48f7eb8753f11ce728c51beb8
NonsensicalNickname <nonsensicalnickname@tutanota.com>
2023-07-30 18:44:07 +1000

    cleaning up

--- RAM.py
+++ RAM.py
@@ -5,11 +5,11 @@ from pyfirmata import Arduino, SERVO
 import platform
 
 
-
 if platform.system() == "Windows":
     port = "COM3"
 else:
     port = "/dev/ttyUSB0"
+
 board = Arduino(port)
 board.digital[2].mode = SERVO
 print("Communication Successfully started")
@@ -32,34 +32,16 @@ while True:
 
 
     success, img = cap.read()
-    hands, img = detector.findHands(img)  # with draw
-
+    hands, img = detector.findHands(img)  
     if hands:
-        # Hand 1
         hand = hands[0]
-        lmList = hand["lmList"]  # List of 21 Landmark points
-        bbox1 = hand["bbox"]  # Bounding box info x,y,w,h
-        centerPoint = hand['center']  # center of the hand cx,cy
-        handType = hand["type"]  # Handtype Left or Right
+        lmList = hand["lmList"] 
+        bbox1 = hand["bbox"] 
+        centerPoint = hand['center'] 
+        handType = hand["type"] 
 
         fingersUp = detector.fingersUp(hand)
-        
-        if len(hands) == 2:
-            # Hand 2
-            hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # List of 21 Landmark points
-            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
-            centerPoint2 = hand2['center']  # center of the hand cx,cy
-            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
 
-            fingers2 = detector.fingersUp(hand2)
-
-            # Find Distance between two Landmarks. Could be same hand or different hands
-            # length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
-            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with dra
-
-
-    # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
     print(f"{handType} hand fingers up: {fingersUp}")
