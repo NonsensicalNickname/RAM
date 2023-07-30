5dd6fc2078f7a296faa5e1644c8e3e0ed32cf9ad
NonsensicalNickname <nonsensicalnickname@tutanota.com>
2023-07-30 18:15:45 +1000

    shit fucking works

--- RAM.py
+++ RAM.py
@@ -4,24 +4,33 @@ import time
 from pyfirmata import Arduino, SERVO
 import platform
 
+
+
 if platform.system() == "Windows":
     port = "COM3"
 else:
     port = "/dev/ttyUSB0"
 board = Arduino(port)
-print("Communication Successfully started")
 board.digital[2].mode = SERVO
-board.digital[2].write(0)
-for i in range(180):
-    angle = i + 1
-    board.digital[2].write(i)
-    print(angle)              
-    time.sleep(0.01) 
-
-'''
+print("Communication Successfully started")
+direction = 2
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 while True:
+    if direction == 0:
+        if board.digital[2].read() > 170:
+            while board.digital[2].read() > 1:
+                board.digital[2].write(board.digital[2].read() - 1)
+                time.sleep(0.01) 
+
+    if direction == 1:
+        if board.digital[2].read() < 10:
+            while board.digital[2].read() < 180:
+                board.digital[2].write(board.digital[2].read() + 1)
+                time.sleep(0.01) 
+
+
+
     success, img = cap.read()
     hands, img = detector.findHands(img)  # with draw
 
@@ -47,13 +56,17 @@ while True:
 
             # Find Distance between two Landmarks. Could be same hand or different hands
             # length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)  # with draw
-            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
-        
-        length, info, img = detector.findDistance(lmList[8], lmList[0], img)
-        print(f"{handType} hand fingers up: {fingersUp}")
+            # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with dra
+
+
     # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+    print(f"{handType} hand fingers up: {fingersUp}")
+    if fingersUp[2] == 0:
+        direction = 0 #down
+    else:
+        direction = 1 #up
+        
 cap.release()
 cv2.destroyAllWindows()
-'''
